0a809d794df08d0917429ba8237de6709ad7bd85
JBPARK21 <bzpark@gmail.com>
2022-10-14 10:17:52 +0900

    message

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,15 +1,35 @@
-//rfc
+import styles from '../../styles/profile.module.css';
+import React from 'react';
 
 export default function profile() {
-  const name = '박범주';
-  const email = 'bzpark@naver.com';
-  const homepage = 'https://www.google.co.kr';
   return (
     <div>
-      <h1>Profile</h1>
-      <p>Name : {name}</p>
-      <p>Email : {email}</p>
-      <p>Homepage : {homepage}</p>
+      <h1 className={styles.h1}>자기소개</h1>
+      <div className={styles.card_box}>
+        <div className={styles.card}>
+          <div className={styles.profile}>
+            <div>
+              <h3 className="text-xl">박범주</h3>
+              Park Beom Ju
+            </div>
+          </div>
+          <p>
+            생년월일 : 1999/09/07
+            <br />
+            <br />
+            학교: 중부대학교 고양캠퍼스
+            <br />
+            학과: 정보보호학전공
+            <br />
+            학번: 91812323 <br />
+            이메일: bzpark@naver.com
+            <br />
+            <br />
+            전공 웹프로젝트 NextShop 입니다. <br />
+            <br />
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
