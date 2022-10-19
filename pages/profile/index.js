bf59ff0339fa4ac3d6ede0cf39cf25543675b0cd
JBPARK21 <bzpark@gmail.com>
2022-10-19 15:33:51 +0900

    message

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 export default function profile() {
   return (
     <div>
-      <h1 className={styles.h1}>자기소개</h1>
       <div className={styles.card_box}>
         <div className={styles.card}>
           <div className={styles.profile}>
@@ -21,13 +20,39 @@ export default function profile() {
             <br />
             학과: 정보보호학전공
             <br />
-            학번: 91812323 <br />
+            학번: 91812323
+            <br />
+            <br />
             이메일: bzpark@naver.com
             <br />
             <br />
             첫 번째 웹사이트 Was 입니다. <br />
             <br />
           </p>
+        </div>
+      </div>
+
+      <h1 className={styles.h1}>자기소개</h1>
+      <h2 className={styles.h2}></h2>
+      <div className={styles.card_box2}>
+        <div className={styles.card2}>
+          <div>
+            중부대학교 고양캠퍼스에서 정보보호학을 전공하고있는 18학번
+            박범주입니다.
+            <br />
+            <br />
+            웹 어플리케이션 보안 수업을 들으며 웹에 대한 지식을 배워가고
+            있습니다.
+            <br />
+            <br />
+            현재 팀원들과 Fall Guys라는 팀으로 한국에 존재하는 다양한 축제
+            문화들을 하나의 사이트에서 정보를 확인할 수 있는 사이트를 제작하고
+            있습니다.
+            <br />
+            <br />
+            팀원들과 한 학기동안 프로젝트를 훌륭히 제 역할을 수행해가며
+            만족스러운 결과물을 낼 수 있도록 팀원들과 노력해가겠습니다.
+          </div>
         </div>
       </div>
     </div>
